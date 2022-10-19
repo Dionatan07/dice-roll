b91a72576428102f334c3f0e8a1a86a7610ecd3b
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-10-19 00:11:53 -0300

    Uso da Collection Map e método Random();

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,26 +16,26 @@ public class Program {
         }
 
         Map<Integer, Integer> plays = new HashMap<>();
-        for (Integer result : values){
-            if(plays.containsKey(result)){
-                plays.put(result, (plays.get(result)+1));
-            }else plays.put(result, 1);
+        for (Integer result : values) {
+            if (plays.containsKey(result)) {
+                plays.put(result, (plays.get(result) + 1));
+            } else plays.put(result, 1);
         }
         System.out.print("Playing");
 
         //Criando um timer antes de mostrar o resultado final
-        for(int i=0; i < 3; i++){
-            try{
+        for (int i = 0; i < 3; i++) {
+            try {
                 Thread.sleep(1250);
                 System.out.print(".");
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
         //Entry utilizado para manipular a Key e o Value separadamente
         System.out.println("\nValue " + " number of times");
-        for (Map.Entry<Integer, Integer> entry : plays.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : plays.entrySet()) {
             //%3d e %10d Estão sendo usados para criar um espaçamento ao mostrar os valores
             System.out.printf("%3d %10d\n", entry.getKey(), entry.getValue());
         }
