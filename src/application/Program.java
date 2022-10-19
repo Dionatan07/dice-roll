a85a307cc75d9c33eb4dae3799e5abadb422fc3e
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-10-18 23:55:39 -0300

    Uso da Collection Map e método Random();

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,24 @@ public class Program {
                 plays.put(result, (plays.get(result)+1));
             }else plays.put(result, 1);
         }
-        System.out.println(plays);
+        System.out.print("Playing");
+
+        //Criando um timer antes de mostrar o resultado final
+        for(int i=0; i < 3; i++){
+            try{
+                Thread.sleep(1250);
+                System.out.print(".");
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        //Entry utilizado para manipular a Key e o Value separadamente
+        System.out.println("\nValue " + " number of times");
+        for (Map.Entry<Integer, Integer> entry : plays.entrySet()){
+            //%3d e %10d Estão sendo usados para criar um espaçamento ao mostrar os valores
+            System.out.printf("%3d %10d\n", entry.getKey(), entry.getValue());
+        }
 
     }
 }
